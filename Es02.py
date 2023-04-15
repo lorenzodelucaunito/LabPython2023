93bcd6970507109338eaca7c24c95b849b674835
JimmyWhite86 <andrea.bianchi086@gmail.com>
2023-04-15 18:14:20 +0200

    Modifiche preliminari
    Aggiunto traccia esercizio e importato la libreria matplotlib

--- Es02.py
+++ Es02.py
@@ -3,10 +3,17 @@
 Created on Tue Apr  4 17:41:33 2023
 
 @author: simone
+
+Esercizio 02:
+    
+    Calcolare il numero di utenti unici che twittano ciascun dominio.
+    Confrontarlo con i valori precedenti.
+    
 """
 import pandas as pd
+import matplotlib.pyplot as plt
 
-
+# Carica i dati dai file CSV
 tweet= pd.read_csv("tweets.csv")
 domini= pd.read_csv("domains.csv")
 
