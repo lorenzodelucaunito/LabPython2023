7dd773aefd743850f9ac448a2dae78cefe5d49d6
lorenzodelucaunito <130917006+lorenzodelucaunito@users.noreply.github.com>
2023-04-16 17:38:50 +0200

    Update Es03.py
    - Aggiunto alla riga 20 un ordinamento dei risultati dal più grande al più piccolo
    - Aggiunto dalla riga 22 alla riga 29 le funzioni per mostrare il grafico a barre

--- Es03.py
+++ Es03.py
@@ -4,7 +4,9 @@ Created on Thu Apr  6 13:06:43 2023
 
 @author: simone
 """
+
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 tweet= pd.read_csv("tweets.csv")
@@ -15,4 +17,14 @@ tweet_medi=tweet_medi[['domain', 'text']].groupby(['domain']).mean().reset_index
 
 tweet_medi = tweet_medi.rename({'text': 'tweet_medi_per_utente'}, axis=1)
 
-display(tweet_medi)
+tweet_medi = tweet_medi.sort_values('tweet_medi_per_utente', ascending=True)
+
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.barh(tweet_medi['domain'], tweet_medi['tweet_medi_per_utente'])
+ax.set_xlabel('Media tweet per utente')
+ax.set_ylabel('Dominio')
+ax.set_title('Media tweet per utente per dominio')
+plt.xticks(rotation=0)
+
+plt.show()
+
