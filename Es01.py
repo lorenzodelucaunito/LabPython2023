ca13d9d1f56f7504d4cda338357b3e70325fe5f4
JimmyWhite86 <andrea.bianchi086@gmail.com>
2023-04-17 14:40:25 +0200

    Esperimenti padding grafici
    Ho provato a fare delle modifiche nel padding delle immagini ma non riesco a farle funzionare.
    Faccio un commit così non mi perdo però il codice anche se non corretto.
    Dopo il commit ritorno alla versione precedente.

--- Es01.py
+++ Es01.py
@@ -37,10 +37,18 @@ print(tweet_dominio)
 #     Creazione del grafico
 # =============================================================================
 
+# Imposto i valori del padding per i sottografici
+padding_top = 1.2       # Padding superiore
+padding_middle = 2     # Padding tra il primo e secondo grafico
+padding_bottom = 3   # Padding inferiore
+
 # Creo una Figure con 3 Axes, uno sotto l'altro
 fig, axs = plt.subplots(nrows = 3, 
                         ncols = 1, 
-                        figsize = (8, 15))
+                        figsize = (10, 15))
+
+# Imposto il primo padding
+#fig.subplots_adjust(hspace=padding_top)
 
 # Creo il grafico per le fake news
 axs[0].bar(tweet_dominio[tweet_dominio['domain_type'] == 'fake news'].domain,
@@ -51,12 +59,19 @@ axs[0].tick_params(axis = 'x', labelrotation = 90)
 # Imposto una scala logaritmica sull'asse delle y per migliorare la leggibilità del grafico
 axs[0].set_yscale('log')
 
+
+# Imposto il secondo padding
+#fig.subplots_adjust(hspace=padding_middle)
+
 # Creo il grafico per le news
 axs[1].bar(tweet_dominio[tweet_dominio['domain_type'] == 'news'].domain,
            tweet_dominio[tweet_dominio['domain_type'] == 'news'].numero_di_tweet,
            color = 'Green')
 axs[1].set_title('News')
 axs[1].tick_params(axis = 'x', labelrotation = 90)
+
+# Imposto il terzo padding
+#fig.subplots_adjust(hspace=padding_bottom)
 
 # Creo il grafico per il fact checjing
 axs[2].bar(tweet_dominio[tweet_dominio['domain_type'] == 'fact checking'].domain,
@@ -66,14 +81,15 @@ axs[2].set_title('Fact Checking')
 #axs[2].tick_params(axis = 'x', labelrotation = -75)
 
 # Aggiungo il titolo globale
-fig.suptitle('Numero di tweet per dominio', fontsize = 14)
+fig.suptitle('Tweet per dominio', fontsize = 14)
 
 # Aggiungo del padding tra gli axes per evitare la sovrapposizione con le etichette dell'asse x
-fig.subplots_adjust(hspace=1.3)
+fig.subplots_adjust(hspace=(0.8, 3, 1))
+
+plt.ylabel('Numero di tweet')
 
 # Salvo la figura prodotta dal grafico
 plt.savefig("Esercizio_01.png")
 
 # Mostro il grafico
 plt.show()
-
