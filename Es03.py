43fb597d6d550f86927ab32696fb16316cabea85
JimmyWhite86 <andrea.bianchi086@gmail.com>
2023-04-17 16:33:52 +0200

    Update Es03.py

--- Es03.py
+++ Es03.py
@@ -16,12 +16,25 @@ tweet_medi=tweet_medi[['domain', 'text']].groupby(['domain']).mean().reset_index
 
 tweet_medi = tweet_medi.rename({'text': 'tweet_medi_per_utente'}, axis=1)
 
+tweet_medi = tweet_medi.sort_values('tweet_medi_per_utente', ascending=True)
+
 print(tweet_medi)
 
+# =============================================================================
+#   Creazione del grafico
+# =============================================================================
 
-plt.bar(tweet_medi['domain'], tweet_medi['tweet_medi_per_utente'])
-plt.xticks(rotation=90) 
-plt.xlabel('Domini',color="red",fontsize=16)
-plt.ylabel('Media numeri di tweet per utente', color="red", fontsize=14)
-plt.title('Media numeri di tweet degli utenti per dominio',color="red", fontweight="bold")
+
+
+
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.barh(tweet_medi['domain'], tweet_medi['tweet_medi_per_utente'])
+ax.set_xlabel('Media tweet per utente')
+ax.set_ylabel('Dominio')
+ax.set_title('Media tweet per utente per dominio')
+plt.xticks(rotation=0)
+
+# Salvo la figura prodotta dal grafico
+plt.savefig("Esercizio_03.png")
+
 plt.show()
